d380c5c92ed594f3884b57885ec74f6acdc2bc91
ajinkya <ajinkya.badve@xpressbees.com>
2017-09-28 13:19:49 +0530

    minor changes done

--- app/src/main/java/iammert/com/androidarchitecture/data/NetworkPageBoundResource.java
+++ app/src/main/java/iammert/com/androidarchitecture/data/NetworkPageBoundResource.java
@@ -12,13 +12,7 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.BiConsumer;
-import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -112,73 +106,58 @@ public abstract class NetworkPageBoundResource<ResultType, RequestType extends R
         Observable.range(1, 20)//for pagination
                 .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
                 // Get each page in order.
-                .concatMap(new Function<Integer, Observable<RequestType>>() {
-                    @Override
-                    public Observable<RequestType> apply(@NonNull Integer integer) throws Exception {
-                        return getPagedObservable(integer).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+                .concatMap(integer -> {
+                    return getPagedObservable(integer).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
 
-//                        return moviesAPI.getPopularMovieAtPage(integer).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
-
-                    }
-                }).doOnComplete(new Action() {
-            @Override
-            public void run() throws Exception {
-                result.removeSource(dbSource);
-
-            }
-        }).doOnError(new Consumer<Throwable>() {
+                }).doOnComplete(() -> result.removeSource(dbSource)).doOnError(new Consumer<Throwable>() {
             @Override
             public void accept(Throwable throwable) throws Exception {
                 onFetchFailed();
                 result.removeSource(dbSource);
                 result.addSource(dbSource, newData -> result.setValue(Resource.error(throwable.getMessage(), newData)));
             }
-        })
-                // Take every result up to and including the one where the next page index is null.
-//                .takeUntil(new Func1<Movie, Boolean>() {
+        }).takeUntil(movie -> {
+            return takeUntil(movie);
+        }).subscribe(requestType -> {
+            if (requestType == null) {
+                return;
+            }
+            List<RequestType> arrayList = new ArrayList<>();
+            arrayList.add(requestType);
+            saveResultAndReInit(arrayList);
+
+        });
+
+
+//                // Add each output to a list builder. I'm using Guava's ImmutableList, but you could
+//                // just as easily use a regular ArrayList and avoid having to map afterwards. I just
+//                // personally prefer outputting an immutable data structure, and using the builder
+//                // is natural for this.
+//                //
+//                // Also, if you wanted to have the observable stream the full output at each page,
+//                // you could use collect instead of reduce. Note it has a slightly different syntax.
+//                .reduce(new ArrayList<RequestType>(), new BiFunction<ArrayList<RequestType>, RequestType, ArrayList<RequestType>>() {
 //                    @Override
-//                    public Boolean call(Movie personsList) {
-//                        return personsList.getPage() == 15;
+//                    public ArrayList<RequestType> apply(ArrayList<RequestType> resultTypes, RequestType resultType) throws Exception {
+//                        resultTypes.add(resultType);
+//                        return resultTypes;
 //                    }
 //                })
-                .takeUntil(new Predicate<RequestType>() {
-                    @Override
-                    public boolean test(@NonNull RequestType movie) throws Exception {
-                        return takeUntil(movie);
-                    }
-                })
-                // Add each output to a list builder. I'm using Guava's ImmutableList, but you could
-                // just as easily use a regular ArrayList and avoid having to map afterwards. I just
-                // personally prefer outputting an immutable data structure, and using the builder
-                // is natural for this.
-                //
-                // Also, if you wanted to have the observable stream the full output at each page,
-                // you could use collect instead of reduce. Note it has a slightly different syntax.
-                .reduce(new ArrayList<RequestType>(), new BiFunction<ArrayList<RequestType>, RequestType, ArrayList<RequestType>>() {
-                    @Override
-                    public ArrayList<RequestType> apply(ArrayList<RequestType> resultTypes, RequestType resultType) throws Exception {
-                        resultTypes.add(resultType);
-                        return resultTypes;
-                    }
-                })
-                // Convert list builder to one List<ResponseObject> of all the things.
-                .map(new Function<ArrayList<RequestType>, CombineClass>() {
-                    @Override
-                    public CombineClass apply(@NonNull ArrayList<RequestType> movies) throws Exception {
-                        return new CombineClass<RequestType>(movies);
-                    }
-                }).subscribe(new BiConsumer<CombineClass, Throwable>() {
-            @Override
-            public void accept(CombineClass testObject, Throwable throwable) throws Exception {
-//                        Log.d(TAG, "accept() called with: testObject = [" + testObject + "], throwable = [" + throwable + "]");
-                if (testObject == null) {
-                    return;
-                }
-                saveResultAndReInit(testObject.getList());
-//                onNextPageLoad(testObject);
-
-            }
-        });
+//                // Convert list builder to one List<ResponseObject> of all the things.
+//                .map(new Function<ArrayList<RequestType>, CombineClass>() {
+//                    @Override
+//                    public CombineClass apply(@NonNull ArrayList<RequestType> movies) throws Exception {
+//                        return new CombineClass<RequestType>(movies);
+//                    }
+//                }).subscribe(new BiConsumer<CombineClass, Throwable>() {
+//            @Override
+//            public void accept(CombineClass testObject, Throwable throwable) throws Exception {
+//                if (testObject == null) {
+//                    return;
+//                }
+//                saveResultAndReInit(testObject.getList());
+//            }
+//        });
     }
 
 }
